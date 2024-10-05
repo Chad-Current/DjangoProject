72500250f51c939a8310b3cf99860e72d670872b
chadcurrent@gmail.com <chadcurrent@gmail.com>
2024-10-05 15:25:02 -0500

    updating url addresses

--- topLevelProject/topLevelProject/urls.py
+++ topLevelProject/topLevelProject/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('baseapp.urls', namespace='home')),
-    path('classes/', include('classes.urls', namespace='classes_main')),
+    path('/classes', include('classes.urls', namespace='classes_main')),
     # path('', include('contactus.urls', namespace='contactus_main')),
     # path('', include('enrollment.urls', namespace='enrollment_main')),
     # path('', include('faqs.urls', namespace='faqs_main')),
