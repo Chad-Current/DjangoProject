4d78494ba8d84ba745dfb9cb6dc082c2e72de660
Chad-Current <chadcurrent@gmail.com>
2024-12-30 16:05:25 -0600

    dropdown and mobile hide js

--- topLevelProject/baseapp/models.py
+++ topLevelProject/baseapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+# class LoginSystem:
+#     def __init__(self):
+#         self.users = dict()
+#         self.logged_users = set()
+#         self.logged_in = set()
+
+#     def register(self,username,password):
+#         # self.username = username
+#         # self.password = password
+#         if username in self.logged_users:
+#             print('user already exists')
+#         else:
+#             self.users[username]=password
+#             self.logged_users.add(username)
+#             print('user registered successfully')
+
+#     def login(self,username,password):
+#         if username not in self.users:
+#             print('user isn\'t in the system')
+#         elif username in self.users:
+#             passcheck = self.users[username]
+#             if passcheck == password:
+#                 self.logged_in.add(username)
+#                 print('user logged in successfully')
+#             else:
+#                 print('password doesn\'t match')
+
+#     def sign_out(self,username):
+#         if not username in self.users:
+#             print('user is not in the system')
+#         elif username not in self.logged_in:
+#             print('user is not logged in')
+#         elif username in self.logged_in:
+#             self.logged_in.remove(username)
+#             print('user signed out successfully')
--- topLevelProject/baseapp/static/baseapp/js/mobile.js
+++ topLevelProject/baseapp/static/baseapp/js/mobile.js
@@ -1,33 +1,46 @@
-const openButton = document.getElementById('open-sidebar-button')
-const navbar = document.getElementById('navbar')
-const media = window.matchMedia("(width < 700px)")
+const openButton = document.getElementById('open-sidebar-button');
+const navbar = document.getElementById('navbar');
+const media = window.matchMedia("(max-width: 700px)");
+const dropdown = document.querySelector('.dropdown');
+const classes = document.querySelector('.classes');
 
-// var menu = document.getElementsByClassName('menu')
+media.addEventListener('change', (e) => updateNavbar(e));
 
-media.addEventListener('change', (e) => updateNavbar(e))
-
-function updateNavbar(e){
-  const isMobile = e.matches
-  console.log(isMobile)
-  if(isMobile){
-    navbar.setAttribute('inert', '')
-  }
-  else{
+function updateNavbar(e) {
+  const isMobile = e.matches;
+  console.log(isMobile);
+  if (isMobile) {
+    navbar.setAttribute('inert', '');
+    hideDropdown();
+  } else {
     // desktop device
-    navbar.removeAttribute('inert')
+    navbar.removeAttribute('inert');
+    showDropdown();
   }
 }
 
-function openSidebar(){
-  navbar.classList.add('show')
-  openButton.setAttribute('aria-expanded', 'true')
-  navbar.removeAttribute('inert')
+function openSidebar() {
+  navbar.classList.add('show');
+  openButton.setAttribute('aria-expanded', 'true');
+  navbar.removeAttribute('inert');
 }
 
-function closeSidebar(){
-  navbar.classList.remove('show')
-  openButton.setAttribute('aria-expanded', 'false')
-  navbar.setAttribute('inert', '')
+function closeSidebar() {
+  navbar.classList.remove('show');
+  openButton.setAttribute('aria-expanded', 'false');
+  navbar.setAttribute('inert', '');
+}
+
+function hideDropdown() {
+  dropdown.style.display = 'none';
+  classes.style.display = 'block';
+  // dropdownContent.style.display = 'none';
+}
+
+function showDropdown() {
+  dropdown.style.display = 'block';
+  classes.style.display = 'none';
+  // dropdownContent.style.display = 'block';
 }
 
 
