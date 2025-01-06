7c7dcf32c4c098cf0d24ff36333b068b0a7424c2
Chad-Current <chadcurrent@gmail.com>
2025-01-06 15:46:02 -0600

    rework

--- topLevelProject/baseapp/static/baseapp/js/index.js
+++ topLevelProject/baseapp/static/baseapp/js/index.js
@@ -1,31 +1,47 @@
 const infograph = document.querySelector('.infographic-container');
+const infograph_mobile = document.querySelector('.infographic-container-mobile');
 const compchart = document.querySelector('.comp-chart');
-
+const compchart_mobile = document.querySelector('.comp-chart-mobile');
 
 function removeInfograph(){
-    if (window.innerWidth <= 780) {
+    if (window.innerWidth <= 768){
         infograph.style.display = "none";
     } else {
         infograph.style.display = "grid";
     }
 }
 
+function removeInfographMobile() {
+    if (window.innerWidth <= 768) {
+        infograph_mobile.style.display = "flex";
+    } else {
+        infograph_mobile.style.display = "none";
+    }
+}
 
 function removeCompchart(){
-    if (window.innerWidth <= 780) {
+    if (window.innerWidth <= 768) {
         compchart.style.display = "none";
     } else {
         compchart.style.display = "flex";
     }
 }
 
+function removeCompchartMobile(){
+    if (window.innerWidth <= 768) {
+        compchart_mobile.style.display = "flex";
+    } else {
+        compchart_mobile.style.display = "none";
+    }
+}
 
-// removeInfograph();
-// removeCompchart();
+
 
 window.addEventListener("resize",function() { 
     removeInfograph();
+    removeInfographMobile();
     removeCompchart();
+    removeCompchartMobile();
 });
 // window.addEventListener("resize", removeInfograph);
 // window.addEventListener("resize", removeCompchart);
